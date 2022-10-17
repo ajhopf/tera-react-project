3a9451eadaba8a620c2f97f65e4b24cd17614d0c
André Hopf <andre.hopf@gmail.com>
2022-10-17 11:05:37 -0300

    Organisms header e footer. Template Default

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import logo from '../../images/logo.svg'
+
+import Default from '../templates/Default'
+
 import user1 from '../../images/placeholders/user-1.jpg'
 import user2 from '../../images/placeholders/user-2.jpg'
 import user3 from '../../images/placeholders/user-3.jpg'
@@ -7,15 +9,7 @@ import user3 from '../../images/placeholders/user-3.jpg'
 export default function Users() {
   return (
   <div className="wrapper">
-    <div className="app-header">
-      <div className="app-header__logo">
-        <img src={logo} className="responsive" alt="" />
-      </div>
-      <div className="app-header__menu">
-        <i className="fa fa-bars"></i>
-      </div>
-    </div>
-
+    <Default>
     <div className="users">
       <h1>Users</h1>
 
@@ -40,8 +34,7 @@ export default function Users() {
         </a>
       </div>
     </div>
-
-    <div className="app-footer">Módulo React :: Full Stack Development</div>
+    </Default>
   </div>
   )
 }
