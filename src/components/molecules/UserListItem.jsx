b6701d185cf51543202853815a73771197b4f73d
André Hopf <andre.hopf@gmail.com>
2022-10-17 14:38:25 -0300

    Avatar em userListItem utilizando o atomo

--- src/components/molecules/UserListItem.jsx
+++ src/components/molecules/UserListItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Avatar from '../atoms/Avatar';
 
 export default function UserListItem(props) {
   return (
     <Link to={{ pathname: `/users/${props.id}` }} className="users__list-item">
       <div className="users__list-item-photo">
-        <img src={props.src} className="responsive avatar" alt="" />
+        <Avatar src={props.src} />
       </div>
       <div className="users__list-item-name">
         {props.fn} {props.ln}
