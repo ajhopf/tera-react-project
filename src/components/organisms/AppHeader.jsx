c3b0368f54a751d9276a0f0e675493a366fc4510
André Hopf <andre.hopf@gmail.com>
2022-10-17 11:59:24 -0300

    Drawer Menu, estados no default

--- src/components/organisms/AppHeader.jsx
+++ src/components/organisms/AppHeader.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import logo from '../../images/logo.svg'
+import { Link } from 'react-router-dom'
 
-export default function AppHeader() {
+export default function AppHeader(props) {
   return (
     <div className="app-header">
       <div className="app-header__logo">
-        <img src={logo} className="responsive" alt="" />
+        <Link to='/'><img src={logo} className="responsive" alt="Tera Logo" /></Link>
       </div>
       <div className="app-header__menu">
-        <i className="fa fa-bars"></i>
+        <i onClick={() => props.setIsOpen(true)} className="fa fa-bars"></i>
       </div>
     </div>
   )
--- src/components/templates/Default.jsx
+++ src/components/templates/Default.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import AppHeader from '../organisms/AppHeader'
 import AppFooter from '../organisms/AppFooter'
-
+import DrawerMenu from '../molecules/DrawerMenu'
 
 export default function Default(props) {
+
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
   <div className="wrapper">
-    <AppHeader />
+    <DrawerMenu open={isOpen} setIsOpen={setIsOpen}/>
+    <AppHeader setIsOpen={setIsOpen}/>
     {props.children}
     <AppFooter />
   </div>
