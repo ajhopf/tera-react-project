049afa92e13e441ed0ef9f675a364bd550c2a6e5
André Hopf <andre.hopf@gmail.com>
2022-10-19 17:07:37 -0300

    Texto escondido por default e handleClick

--- src/components/molecules/PostListItem.jsx
+++ src/components/molecules/PostListItem.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 export default function PostListItem(props) {
+  const [showContent, setShowContent] = React.useState(false);
+
+  const handleShowContent = () => {
+    setShowContent(!showContent);
+  };
+
   return (
-    <div className="user-blog__posts-item">
+    <div onClick={handleShowContent} className="user-blog__posts-item">
       <div className="user-blog__posts-item-photo">
         <img src={props.image} className="responsive" alt="" />
       </div>
       <h2 className="user-blog__posts-item-title">{props.title}</h2>
       <div className="user-blog__posts-date">{props.createdAt}</div>
-      <div className="user-blog__post-content">{props.content}</div>
+      {showContent && (
+        <div className="user-blog__post-content">{props.content}</div>
+      )}
     </div>
   );
 }
