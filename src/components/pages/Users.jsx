f3f28162f29fad890aa823b17a49edd5f2a9b904
André Hopf <andre.hopf@gmail.com>
2022-10-19 17:15:32 -0300

    Loading in users

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Default from '../templates/Default';
+import AppLoading from '../organisms/AppLoading';
 
 import UserListWrapper from '../molecules/UserListWrapper';
 
 export default function Users(props) {
   const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     const endpoint = `https://62c4e487abea8c085a7e022a.mockapi.io/users`;
@@ -14,10 +16,13 @@ export default function Users(props) {
       .then(response => response.json())
       .then(data => {
         setUsers(data);
+        setIsLoading(false);
       });
   }, []);
 
-  return (
+  return isLoading ? (
+    <AppLoading />
+  ) : (
     <div className="wrapper">
       <Default>
         <div className="users">
